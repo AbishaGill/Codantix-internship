5fff9755b5ccf61bc74340b3c30a27bc564c1b42
AbishaGill <gillabisha4@gmail.com>
2025-08-11 07:10:41 -0700

    Add files via upload

---
+++ Task#4/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="bg-[#001D3D] text-white text-center py-4 ">
+      <p class="text-sm">&copy; 2025 Abisha Iqbal Gill. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
