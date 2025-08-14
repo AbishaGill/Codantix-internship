b9e00541c75c669b60f2059febf727ef2a8292a5
AbishaGill <gillabisha4@gmail.com>
2025-08-14 03:23:21 -0700

    Delete Task 5 directory

--- Task 5/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-const Button = () => {
-  const [isDark, setIsDark] = useState(false);
-  return <div className="border rounded-lg p-2 cursor-pointer">Button</div>;
-};
-
-export default Button;
